3103f10a483764f2badf5f9be9addc5a26606ae9
hilla10 <hillaman592@gmail.com>
2025-06-07 17:39:18 +0300

    change Navbar to navbar in index.js file

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as Header } from './header/Header';
-export { default as Navbar } from './Navbar/Navbar';
+export { default as Navbar } from './navbar/Navbar';
 export { default as About } from './about/About';
 export { default as Experience } from './experience/Experience';
 export { default as Services } from './services/Services';
